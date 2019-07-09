9e6255c46f840278d69b9cab95dcf9793d89a728
test <tes@abc.com>
2019-07-09 15:58:28 +0700

    Fonts Success

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
-
 class Authen extends StatefulWidget {
   @override
   _AuthenState createState() => _AuthenState();
 }
 
 class _AuthenState extends State<Authen> {
+// Explicit
+
+// Method
+  Widget showLogo() {
+    return Container(
+      width: 180.0,
+      height: 180.0,
+      child: Image.asset(
+        'images/logo.png',
+      ),
+    );
+  }
+
+  Widget showText() {
+    return Text(
+      'A TPA',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.lightGreenAccent[700],
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Notable',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Authen'),);
+    return Scaffold(
+      body: Container(
+          padding: EdgeInsets.only(top: 60.0),
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              showLogo(),
+              showText(),
+            ],
+          )),
+    );
   }
 }
