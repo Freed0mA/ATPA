ffce4481664924f4c74f7149ece73e85f7b807c3
test <tes@abc.com>
2019-07-10 10:14:11 +0700

    TextFormField Sucess

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:a_tpa/screens/authen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 
 main() {
   runApp(MyApp());
@@ -9,6 +9,8 @@ main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       home: Authen(),
     );
