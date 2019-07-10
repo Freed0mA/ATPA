8ed23dafd239e3641b205e7d473b0f3ec9706457
test <tes@abc.com>
2019-07-10 14:09:52 +0700

    Design UI Register Success

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -6,12 +7,112 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+// Explicit
+
+// Method
+  Widget uploadButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {},
+    );
+  }
+
+  Widget nameText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Name :',
+        labelStyle: TextStyle(
+          color: Colors.blue,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+        helperText: 'FirstName and LastName',
+        helperStyle: TextStyle(
+          color: Colors.blue[200],
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        ),
+        icon: Icon(
+          Icons.account_box,
+          size: 36.0,
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  Widget emailText() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email :',
+        labelStyle: TextStyle(
+          color: Colors.green,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+        helperText: 'you@email.com',
+        helperStyle: TextStyle(
+          color: Colors.green[200],
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        ),
+        icon: Icon(
+          Icons.mail,
+          size: 36.0,
+          color: Colors.green,
+        ),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password :',
+        labelStyle: TextStyle(
+          color: Colors.red,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+        helperText: 'More 6 Charactor',
+        helperStyle: TextStyle(
+          color: Colors.red[200],
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        ),
+        icon: Icon(
+          Icons.vpn_key,
+          size: 36.0,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
+        actions: <Widget>[uploadButton()],
+        backgroundColor: Colors.green,
         title: Text('Register'),
-      ),body: Text('body'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 30.0),
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: 250.0,
+          child: Column(
+            children: <Widget>[
+              nameText(),
+              emailText(),
+              passwordText(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
